c420aec28e62d44a2f98d85c476b7db59bbf7b2d
crypticowl <yadavashutosh708@gmail.com>
2024-12-20 14:39:07 +0530

    Uncommented socket code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,26 +8,28 @@ public class Main {
     System.out.println("Logs from your program will appear here!");
 
     //  Uncomment this block to pass the first stage
-    //    ServerSocket serverSocket = null;
-    //    Socket clientSocket = null;
-    //    int port = 6379;
-    //    try {
-    //      serverSocket = new ServerSocket(port);
-    //      // Since the tester restarts your program quite often, setting SO_REUSEADDR
-    //      // ensures that we don't run into 'Address already in use' errors
-    //      serverSocket.setReuseAddress(true);
-    //      // Wait for connection from client.
-    //      clientSocket = serverSocket.accept();
-    //    } catch (IOException e) {
-    //      System.out.println("IOException: " + e.getMessage());
-    //    } finally {
-    //      try {
-    //        if (clientSocket != null) {
-    //          clientSocket.close();
-    //        }
-    //      } catch (IOException e) {
-    //        System.out.println("IOException: " + e.getMessage());
-    //      }
-    //    }
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        int port = 6379;
+        try {
+            System.out.println("Port: "+port);
+          serverSocket = new ServerSocket(port);
+          // Since the tester restarts your program quite often, setting SO_REUSEADDR
+          // ensures that we don't run into 'Address already in use' errors
+          serverSocket.setReuseAddress(true);
+          // Wait for connection from client.
+          clientSocket = serverSocket.accept();
+            System.out.println("Server: "+serverSocket+"\n clientSocket: "+clientSocket);
+        } catch (IOException e) {
+          System.out.println("IOException: " + e.getMessage());
+        } finally {
+          try {
+            if (clientSocket != null) {
+              clientSocket.close();
+            }
+          } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+          }
+        }
   }
 }
