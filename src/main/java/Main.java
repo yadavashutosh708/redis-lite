a45085ede63d6576cf281661d479c715989a0342
crypticowl <yadavashutosh708@gmail.com>
2024-12-22 14:18:48 +0530

    Added PING command fixes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -23,21 +23,29 @@ public class Main {
           // Wait for connection from client.
           clientSocket = serverSocket.accept();
             System.out.println("Server: "+serverSocket+"\n clientSocket: "+clientSocket);
+
+
+            InputStream inputStream = clientSocket.getInputStream();
+            String text = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8).trim();
+            System.out.println("Received Command: "+ text);
+            if(text.equals("*1\\r\\n$4\\r\\nping\\r\\n")){
+                OutputStream outputStream = clientSocket.getOutputStream();
+                String output = "PONG";
+                outputStream.write(output.getBytes(StandardCharsets.UTF_8));
+                outputStream.flush();
+
+                System.out.println("Sent Respinse: "+ output);
+                outputStream.close();
+                inputStream.close();
+            }else{
+                System.out.println("Not met");
+            }
+
         } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
         } finally {
           try {
             if (clientSocket != null) {
-              InputStream inputStream = clientSocket.getInputStream();
-              String text = new String(inputStream.readAllBytes());
-              inputStream.close();
-              if(text.trim().equals("PING")){
-                  OutputStream outputStream = clientSocket.getOutputStream();
-                  String output = "PONG";
-                  outputStream.write(output.getBytes(StandardCharsets.UTF_8));
-                  outputStream.flush();
-                  outputStream.close();
-              }
               clientSocket.close();
             }
           } catch (IOException e) {
